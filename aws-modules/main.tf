9f02cde29b3f4d5ac8ddf57004312a06c3f06132
Rikesh Man Dangol <49281273+rik7614@users.noreply.github.com>
2022-07-25 11:42:48 +0100

    Delete aws-module

--- aws-modules/main.tf
+++
@@ -1,9 +0,0 @@
-module "vpc" {
-  source = "./module/vpc"
-  cidr_range = "10.0.0.0/16"
-  domain_name = "hrs.com"
-  number_of_ngw = 2
-  azs = ["us-west-2a, us-west-2b, us-west-2c"]
-  public_cidr_ranges = ["172.18.0.0/22","172.18.4.0/22","172.18.8.0/22"]
-  private_cidr_ranges= ["10.192.0.0/18","10.192.64.0/18","10.192.128.0/18"]
-}
